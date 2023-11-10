1946da46e2e93ec994dc57a8e17c9c83102c2317
Emii2003 <emile.maia@etec.sp.gov.br>
2023-11-10 17:20:17 -0300

    Começando o sistema de login

--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -1,7 +1,20 @@
-import React from "react"
+"use client"
+import { useState } from "react"
 import "./cadastro.css"
 
-export default function Cadastro(){
+export default function CadastroPage(){
+    const [formData, setFormData] = useState({
+        email: '',
+        senha: '',
+        cpf: '',
+    })
+
+    const handleFormEdit = (event, name) => {
+        setFormData({
+            ...formData,
+            [name]: event.target.value
+        })
+    }
     return(
         <div className="content_cadastro">
             <form>
@@ -9,23 +22,24 @@ export default function Cadastro(){
 
                 <div className="form_group">
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
+                    <input type="email" id="email" name="email" required value = {formData.email}
+                    onChange = {(e) => {handleFormEdit(e, 'name')}}/>
                 </div>
 
                 <div className="form_group">
                     <label htmlFor="senha">Senha:</label>
-                    <input type="password" id="senha" name="senha" required />
+                    <input type="password" id="senha" name="senha" required value = {formData.senha}/>
                 </div>
 
                 <div className="form_group">
                     <label htmlFor="cpf">CPF:</label>
-                    <input type="text" id="cpf" name="cpf" required />
+                    <input type="text" id="cpf" name="cpf" required value = {formData.cpf}/>
                 </div>
         
-                <div className="form_group">
+                {/* <div className="form_group">
                     <label htmlFor="dataNascimento">Data de Nascimento:</label>
                     <input type="date" id="dataNascimento" name="dataNascimento" required />
-                </div>
+                </div> */}
 
                 <input type="submit" value="Cadastrar" id="button"/>
             </form>
