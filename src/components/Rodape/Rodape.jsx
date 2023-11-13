3deb524321b02e9a1975a2ff0bbb3d86c4d847ff
Str1nder <rm552238@fiap.com.br>
2023-11-13 18:52:27 -0300

    Ajustando icones de perfil na home

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -6,12 +6,6 @@ export default function Footer(){
     return(
         <footer className="footer">
         <div className="footer-content">
-          <nav className="footer-links">
-            <ul>
-              <li><Link href = '/sobre'>Sobre Nós</Link></li>
-              <li><Link href = '/projeto'>Nosso Projeto</Link></li>
-            </ul>
-          </nav>
           <p className="footer-text">Realização - Thinking Five</p>
         </div>
       </footer>
